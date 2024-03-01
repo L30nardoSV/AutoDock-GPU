6ff68cbb8904abbe1f5acb5309e2b9323cbe8f49
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2024-03-01 09:37:15 -0500

    Plotting vdw, el, and desol in three subplots.

--- plot_vdw.py
+++ plot_vdw.py
@@ -29,37 +29,76 @@ with open(filename) as f:
 
 # Identifying lengths, mins, and maxs
 len_vdw = len(vdw); min_vdw = min(vdw); max_vdw = max(vdw)
-print("len_vdw =", len_vdw, "\nmin_vdw =", min_vdw, "\nmax_vdw =", max_vdw)
+len_el = len(el); min_el = min(el); max_el = max(el)
+len_desol = len(desol); min_desol = min(desol); max_desol = max(desol)
+print("len_vdw =", len_vdw,   "\tmin_vdw =", min_vdw,   "\tmax_vdw =", max_vdw)
+print("len_el  =", len_el,    "\tmin_el  =", min_el,    "\tmax_el  =", max_el)
+print("len_des =", len_desol, "\tmin_des =", min_desol, "\tmax_des =", max_desol)
 
 x = list(range(0, len_vdw))
 len_x = len(x); min_x = min(x); max_x = max(x)
-print("len_x =", len_x, "\nmin_x =", min_x, "\nmax_x =", max_x)
 
-assert len_vdw == len_x, "Number of elements is expected to be the same for both axes"
-#print(x); print(y)
+assert len_x == len_vdw == len_el == len_desol, "Number of elements is expected to be the same for both axes"
 
 # Printing only min and max in ticks
 t_x = [min_x, max_x]
 t_vdw = [min_vdw, max_vdw]
-print(t_x); print(t_vdw)
+t_el = [min_el, max_el]
+t_desol = [min_desol, max_desol]
+
+min_all = min(min_vdw, min_el, min_desol)
+max_all = max(max_vdw, max_el, max_desol)
+t_all = [min_all, max_all]
+print("\t\tmin_all =", min_all, "\tmax_all =", max_all)
 
 # Plotting
-fig, ax = plt.subplots()
-fig = plt.plot(x, vdw, marker='o')
-ax.set_xticks(t_x)
-ax.set_yticks(t_vdw)
-ax.yaxis.set_major_formatter(FormatStrFormatter('%.7f'))
+fig, axs = plt.subplots(3, sharex=True)
+axs[0].plot(x, vdw, color = 'red', marker='o')
+axs[0].set_title('Van der Waals')
+axs[0].set_ylabel('Energy (kcal/mol)')
+axs[0].set_xticks(t_x)
+axs[0].set_yticks(t_vdw)
+axs[0].xaxis.set_major_formatter(FormatStrFormatter('%d'))
+axs[0].yaxis.set_major_formatter(FormatStrFormatter('%.7f'))
+
+axs[1].plot(x, el, color = 'blue', marker='x')
+axs[1].set_title('Electrostatic')
+axs[1].set_ylabel('Energy (kcal/mol)')
+axs[1].set_yticks(t_el)
+axs[1].yaxis.set_major_formatter(FormatStrFormatter('%.7f'))
+
+axs[2].plot(x, desol, color = 'green', marker='4')
+axs[2].set_title('Desolvation')
+axs[2].set_ylabel('Energy (kcal/mol)')
+axs[2].set_yticks(t_desol)
+axs[2].yaxis.set_major_formatter(FormatStrFormatter('%.7f'))
 
 # Annotating min and max coordinates
-xmax = x[np.argmax(vdw)]
-text = "vdw = {:.7f} \nsample = {}".format(max_vdw, xmax)
-ax.annotate(text, xy=(xmax, max_vdw), xytext=(xmax+10, max_vdw+40))
+xmax_vdw = x[np.argmax(vdw)]
+text = "vdw = {:.7f} \nsample = {}".format(max_vdw, xmax_vdw)
+axs[0].annotate(text, xy=(xmax_vdw, max_vdw))
 
-xmin = x[np.argmin(vdw)]
-text = "vdw = {:.7f} \nsample = {}".format(min_vdw, xmin)
-ax.annotate(text, xy=(xmin, min_vdw), xytext=(xmin+10, min_vdw+40))
+xmin_vdw = x[np.argmin(vdw)]
+text = "vdw = {:.7f} \nsample = {}".format(min_vdw, xmin_vdw)
+axs[0].annotate(text, xy=(xmin_vdw, min_vdw))
 
+xmax_el = x[np.argmax(el)]
+text = "el = {:.7f} \nsample = {}".format(max_el, xmax_el)
+axs[1].annotate(text, xy=(xmax_el, max_el))
+
+xmin_el = x[np.argmin(el)]
+text = "el = {:.7f} \nsample = {}".format(min_el, xmin_el)
+axs[1].annotate(text, xy=(xmin_el, min_el))
+
+xmax_desol = x[np.argmax(desol)]
+text = "desol = {:.7f} \nsample = {}".format(max_desol, xmax_desol)
+axs[2].annotate(text, xy=(xmax_desol, max_desol))
+
+xmin_desol = x[np.argmin(desol)]
+text = "desol = {:.7f} \nsample = {}".format(min_desol, xmin_desol)
+axs[2].annotate(text, xy=(xmin_desol, min_desol))
+
+# Label in x axis is shared
 plt.xlabel("Samples")
-plt.ylabel("Energy (kcal/mol)")
 
 plt.show()
